2a1570058d1007f57c67dc9b915c2ec945602427
tonico2011 <huyco2011@gmail.com>
2019-06-14 00:13:13 +1000

    Flip-Tutorial01

--- MatLab intro - Textbook/ch2p3.m
+++ MatLab intro - Textbook/ch2p3.m
@@ -1,4 +1,5 @@
 '(ch2p3)' % Display label.
-P2=[3 5 7 8] ; % Assign 3s^3 + 5s^2 +7s + 8 to P2
+P2=[1 2 20] ; % Assign 3s^3 + 5s^2 +7s + 8 to P2
 % without displaying.
-3*5 % Evaluate 3*5 and display result.
+ % Evaluate 3*5 and display result.
+roots(P2)   
---
+++ Tutorial01.m
@@ -0,0 +1,6 @@
+%%Question 10
+
+num = [5000];
+deno = [1 20 1000 5000];
+
+[r,p,k] = residue(num,deno)
