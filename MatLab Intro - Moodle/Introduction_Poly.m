9082f711b0f61c5960c47d127d59b6c86fc04c13
tonico2011 <huyco2011@gmail.com>
2019-06-13 13:23:35 +1000

    MATLAB-02: Introduction poly

--- MatLab Intro - Moodle/Introduction_Poly.m
+++ MatLab Intro - Moodle/Introduction_Poly.m
@@ -2,11 +2,32 @@
 clc; clear
 
 % s^2+4*s+4
-b = [1 4 4]
+b = [1 4 4];
 
 % s^3+7*s^2+12*s
 
-c= [1 7 12 0]
+c= [1 7 12 0];
 
+% s^3+1
+d = [1 0 0 1];
 
-%%
+% Finding roots
+r1 = roots(b)
+r2 = roots(c)
+r3 = roots(d)
+
+%% Evaluating polynomial for a give value
+% Poly value of b at 1
+polyval(b, 1)
+
+% Multiplying polynomials
+D = conv(b,c)
+
+% Dividing two polynomials (devide D by b, expect to get c)
+[q, r] = deconv(D, b)
+
+% Converting roots to polynomial(s)
+e = [-2 -2]
+p1 = poly(e)
+p1 = poly(r2)
+
