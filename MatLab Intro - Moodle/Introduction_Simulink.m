9a9feccf72b3ac89e6622986143191a0cefbaff5
tonico2011 <huyco2011@gmail.com>
2019-06-13 15:28:00 +1000

    MatLab3114 - 04: Simulink Intro

---
+++ MatLab Intro - Moodle/Introduction_Simulink.m
@@ -0,0 +1,12 @@
+%% Loading and running a Simulink model
+clc; clear; close all;
+
+% Opening Simulink
+simulink
+
+% Opening Simulink library
+%  slLibraryBrowser
+
+% Define value for gains a1 & a2
+a1 = 2; 
+a2 = 3;
