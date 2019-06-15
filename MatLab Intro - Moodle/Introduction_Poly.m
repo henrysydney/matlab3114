d9ea80e5609536f1255db2a98927fbaa149c1ae1
tonico2011 <huyco2011@gmail.com>
2019-06-15 10:09:02 +1000

    LTI SYSTEM

--- MatLab Intro - Moodle/Introduction_Poly.m
+++ MatLab Intro - Moodle/Introduction_Poly.m
@@ -3,40 +3,48 @@
 clc; clear
 
 % s^2+4*s+4
-b = [1 4 4];
+   b = [1 4 4];
 
 % s^3+7*s^2+12*s
 
-c= [1 7 12 0];
+   c= [1 7 12 0];
 
 % s^3+1
-d = [1 0 0 1];
+   d = [1 0 0 1];
 
 % Finding roots
-r1 = roots(b)
-r2 = roots(c)
-r3 = roots(d)
+   r1 = roots(b)
+   r2 = roots(c)
+   r3 = roots(d)
 
 %% Evaluating polynomial for a give value
 % Poly value of b at 1
-polyval(b, 1)
+   polyval(b, 1)
 
 % Multiplying polynomials
-D = conv(b,c)
-roots(D)
+   D = conv(b,c)
+   roots(D)
 % Dividing two polynomials (devide D by b, expect to get c)
-[q, r] = deconv(D, b);
+   [q, r] = deconv(D, b);
 
 % Converting roots to polynomial(s)
-e = [-2 -2];
-p1 = poly(e);
-p1 = poly(r2);
+   e = [-2 -2];
+   p1 = poly(e);
+   p1 = poly(r2);
 
-f = [0 -3 -4];
-p2 = poly(f);
+   f = [0 -3 -4];
+   p2 = poly(f);
 
-g = [-1+2*j -1-2*j];
-p3 = poly(g)
+   g = [-1+2*j -1-2*j];
+   p3 = poly(g)
 
+%% Partial Fraction expansion
+   [R, P, K] = residue([1 4 4],[1 7 12 0]);
+
+   %1/s*(s^2+2*s+5)
+   G = tf(1, [1 2 5 0])
+   [R,P,K] = residue(1, [1 2 5 0])
+
+   % help laplace
 
 
---
+++ MatLab Intro - Moodle/LTI_SYSTEMs.m
@@ -0,0 +1,73 @@
+%% Loading and running a Simulink model
+   clc; clear; close all;
+
+% Opening Simulink
+   simulink
+
+% Opening Simulink library
+%  slLibraryBrowser
+
+% Define value for gains a1 & a2
+   a1 = 2; 
+   a2 = 3;
+
+%% Stability, step/inputs response plotting and closed-loop systems
+   clc; clear ;
+
+   s = tf('s')
+   G1 = 1/(s^2+6*s+5);
+   zero(G1);
+   pole(G1);
+   DCGain = dcgain(G1);
+
+  % figure(1);
+   step(G1);
+   stepinfo(G1);
+
+  % figure(2);
+   impulse(G1);
+   stepinfo(impulse(G1));
+   
+ % Closed loop system
+   %help feedback
+   %           G1 is transfer function, 1 is feedback
+   
+   T1 = feedback(G1, 1);
+   % Check whether the system is stable (T1)
+   pT1 = pole(T1)
+  % figure(3);
+   % Compare closed looop vs open loop system
+   step(T1, G1);
+   legend('T1 Closed Loop', 'G1 Open Loop');
+   
+  % Multiple step response for overdamped, underdamped, and critically
+  % Damped systems
+  
+  K2 = 4;
+  T2 = feedback(K2*G1, 1)
+  pT2 = pole(T2)
+  
+  K3 = 12;
+  T3 = feedback(K3*G1, 1)
+  pT2 = pole(T3)
+  
+  figure(4)
+  step(T1, T2, T3)
+  legend('Overdamped K = 1 ', 'Critically damped K = 4 ', 'Underdamped K = 12');
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
