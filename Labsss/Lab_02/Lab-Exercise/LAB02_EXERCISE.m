76aa96dff5922f970a6af9d9edde6e018526ae50
tonico2011 <huyco2011@gmail.com>
2019-06-28 09:33:17 +1000

    exercise 2

--- Labsss/Lab_02/Lab-Exercise/LAB02_EXERCISE.m
+++ Labsss/Lab_02/Lab-Exercise/LAB02_EXERCISE.m
@@ -1,4 +1,4 @@
-% Lab exercise 02
+%% Lab exercise 02
 
 % Data is stored in discSpeed
     
@@ -17,5 +17,50 @@
 % Fit2 = Acutal data Yact vs. Manufacture Speed (Yman)
   Fit2 = (1 - norm(Yact - Yman)/norm(Yact - mean(Yact)))*100
   
-% As the value of Fit2 > Fit1, there fore Identified Function performs
-% better
+% As the value of Fit2 > Fit1; thus, Manufacture Model performs better than the Indentified Model
+
+%% A mass (Ring) is attached
+clear all; close all; clc;
+
+% Import require data
+ run 'ELEC3114_Lab2.m';
+ load 'speed_with_ring.mat';
+
+% Assign value
+%  timeValue = Speed(:, 1);
+    newDisc = Speed;                  % make a copy of input data
+
+% As we prefer to use Manufacture Mode, 
+%  Yact = Speed(:, 3);    % Actual speed (Yact)
+%  Yman = Speed(:, 5);    % Speed is filter - Manufacture Model
+
+% Truncated & offset data
+% Trunacating the first second to find the offset
+
+    time_remove = newDisc(:, 1) < 1;     % For which rows is column 1 less than 1
+    newDisc(time_remove, :) = [];        % Remove those rows
+    time1 = newDisc(:, 1) - 1;           % Shifting the time
+    speed1 = newDisc(:, 5);   
+    a = speed1(1:1000,1);                  % Storing data of the 1st second
+    lowest = min(a(:));                    
+    highest = max(a(:));
+    m = (lowest+highest)/2;                % Find average of highest and lowest of the 1st secondd
+
+   newDisc1 = Speed;
+   time_remove4 = newDisc1(:, 1) < 2;     % For which rows is column 1 less than 2
+   newDisc1(time_remove4, :) = [];        % Remove those rows
+   time4 = newDisc1(:, 1) - 2;            % Shifting the time
+   speed4 = newDisc1(:, 5);
+   speed4 = newDisc1(:, 5) - m;
+
+
+   figure;
+   plot(time4, speed4);
+   xlim([0 10])
+   ylim([0 600])
+   grid ON;
+   xlabel('Time (sec)');
+   ylabel('Disc Speed (rpm)');
+   title('Offset-free Speed Data');
+
+
